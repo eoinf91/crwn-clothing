7578532c003f0fd6343e854b39c34cca8696a3a8
Eoin <me@eoinfeely.com>
2020-01-07 15:32:47 +0100

    updaing stripe integration and updating our favicon and title

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -9,6 +9,7 @@ import './checkout.styles.scss';
 
 // Components
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
 
 const CheckoutPage = ({ cartItems, total }) => (
     <div className='checkout-page'>
@@ -35,9 +36,13 @@ const CheckoutPage = ({ cartItems, total }) => (
             ))
         }
 
-        <div className='total'>
-            <span>Total: ${total}</span>
+        <div className='total'>Total: ${total}</div>
+        <div className='test-warning'>
+            *Please use the following test credit card for payments*
+            <br />
+            4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
         </div>
+        <StripeCheckoutButton price={total} />
     </div>
 );
 
